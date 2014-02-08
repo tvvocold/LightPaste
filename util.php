32b5983d4cb778e553f04040a044d4d0a24c6e20
Kenny Shields <nikolairesokav@gmail.com>
2014-02-08 05:11:16 -0500

    Add util::formatDataSize

--- util.php
+++ util.php
@@ -1,6 +1,28 @@
 <?php
 
 	class util {
+		/*=======================================================
+			func: formatDataSize($bytes)
+			desc: formats the given data size into a string
+			source: http://stackoverflow.com/questions/5501427/php-filesize-mb-kb-conversion
+		=======================================================*/
+		static function formatDataSize($bytes)
+		{
+			if($bytes >= 1073741824) {
+				$bytes = number_format($bytes / 1073741824, 2) . ' GB';
+			} elseif($bytes >= 1048576) {
+				$bytes = number_format($bytes / 1048576, 2) . ' MB';
+			} elseif($bytes >= 1024) {
+				$bytes = number_format($bytes / 1024, 2) . ' KB';
+			} elseif($bytes > 1) {
+				$bytes = $bytes . ' bytes';
+			} elseif($bytes == 1) {
+				$bytes = $bytes . ' byte';
+			} else {
+				$bytes = '0 bytes';
+			}
+			return $bytes;
+		}
 	}
 	
 ?>
