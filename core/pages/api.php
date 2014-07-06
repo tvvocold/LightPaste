036152bd4ac8e4234d29e2f959a399fe3bd8ae91
Kenny Shields <nikolairesokav@gmail.com>
2014-07-06 17:10:31 -0400

    Add list of post request values to the api page

--- core/pages/api.php
+++ core/pages/api.php
@@ -1,8 +1,12 @@
 <?php
-
+	
 	$f3->route("GET /api", function($f3) {
+		global $DATA_LANGUAGES;
+		global $DATA_EXPIRATIONS;
 		util::getClientSettings($f3);
 		$f3->set("page_title", "Light Paste / API");
+		$f3->set("api_languages", $DATA_LANGUAGES);
+		$f3->set("api_expirations", $DATA_EXPIRATIONS);
 		$template = new Template;
 		echo $template->render("templates/api.html");
 	});
